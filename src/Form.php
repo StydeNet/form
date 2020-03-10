c73681140324debe5c9b6f53d4b5928f37f35f51
Duilio Palacios <duilio@styde.net>
2020-03-10 14:35:04 +0000

    Create Form class

---
+++ src/Form.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Styde;
+
+class Form
+{
+    public function render()
+    {
+        return '<form></form>';
+    }
+}
---
+++ tests/FormTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use Styde\Form;
+
+class FormTest extends TestCase
+{
+    /** @test */
+    function renders_a_form()
+    {
+        $form = new Form;
+
+        $this->assertSame('<form></form>', $form->render());
+    }
+}
