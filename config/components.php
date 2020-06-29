cd9bade18ed9923e458ee1aa7f1d5cbe1f6b91d5
Rafael Torrealba <rafaeltorrealba6@gmail.com>
2020-06-29 19:29:58 -0400

    adding submit button

--- config/components.php
+++ config/components.php
@@ -11,9 +11,5 @@ return [
     \Styde\Form\RadioField::class => 'field-radio',
     \Styde\Form\TextAreaField::class => 'field-textarea',
     \Styde\Form\FileField::class => 'field-file',
-    //\Styde\Form\FileBrowserField::class => 'field-file-browser',
-    //\Styde\Form\RangeField::class => 'field-range',
-    //\Styde\Form\SwitchField::class => 'field-switch',
-    //\Styde\Form\SubmitButton::class => 'submit',
-    //
+    \Styde\Form\SubmitButton::class => 'submit'
 ];
