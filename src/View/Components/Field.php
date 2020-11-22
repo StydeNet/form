06928232b0d3d2e13332248847593c625b8da68d
Luis Arce <luilliarcec@gmail.com>
2020-11-22 15:51:02 -0500

    Creating private properties of Config and Cache (Repository)

--- src/View/Components/Field.php
+++ src/View/Components/Field.php
@@ -2,10 +2,18 @@
 
 namespace Styde\Form\View\Components;
 
+use Illuminate\Cache\Repository as Cache;
+use Illuminate\Config\Repository as Config;
 use Illuminate\View\Component;
 
 abstract class Field extends Component
 {
     /** @var string CSS Class for control */
     protected $classes = 'form-control';
+
+    /** @var Config */
+    private $config;
+
+    /** @var Cache */
+    private $cache;
 }
