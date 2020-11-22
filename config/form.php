c1658bd2504e1ac28873e4b71ccf9d0c0a067864
Luis Arce <luilliarcec@gmail.com>
2020-11-22 15:19:37 -0500

    Adding customizable highlights for required and optional

--- config/form.php
+++ config/form.php
@@ -1,5 +1,19 @@
 <?php
 
 return [
+    'highlights' => [
+        'required' => [
+            'activated' => true,
+            'color-badge' => 'danger',
+            'text' => 'styde-form::field.required'
+        ],
+
+        'optional' => [
+            'activated' => false,
+            'color-badge' => 'info',
+            'text' => 'styde-form::field.optional'
+        ]
+    ],
+
     'highlights_requirement' => 'optional',
 ];
