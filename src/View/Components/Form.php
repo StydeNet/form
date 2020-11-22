11b95928d97225e69afc6eef43b973f725fd02b1
Luis Arce <luilliarcec@gmail.com>
2020-11-22 15:39:18 -0500

    Injecting Cache Repository and receiving the Model in the form.

--- src/View/Components/Form.php
+++ src/View/Components/Form.php
@@ -2,20 +2,58 @@
 
 namespace Styde\Form\View\Components;
 
+use Illuminate\Cache\Repository as Cache;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\View\Component;
 
 class Form extends Component
 {
-    /**
-     * @var string
-     */
+    /** @var string Form method */
     public $method;
 
-    public function __construct(string $method = 'get')
+    /** @var Cache Model */
+    private $cache;
+
+    /**
+     * Form constructor.
+     *
+     * @param Cache $cache
+     * @param string $method
+     * @param Model|null $model
+     */
+    public function __construct(Cache $cache, string $method = 'get', Model $model = null)
     {
         $this->method = $method;
+        $this->cache = $cache;
+
+        if ($model) {
+            $this->cache->put('b-model', $model);
+        }
     }
 
+    /**
+     * Form destructor.
+     */
+    public function __destruct()
+    {
+        $this->cache->forget('b-model');
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return view('styde-form::form');
+    }
+
+    /**
+     * Returns the http method that will go in the form html
+     *
+     * @return string
+     */
     public function httpMethod()
     {
         if ($this->spoofedMethod()) {
@@ -25,13 +63,13 @@ class Form extends Component
         return $this->method;
     }
 
+    /**
+     * Check if the method is put patch or delete
+     *
+     * @return bool
+     */
     public function spoofedMethod()
     {
         return in_array($this->method, ['put', 'patch', 'delete']);
-    }
-
-    public function render()
-    {
-        return view('styde-form::form');
     }
 }
