f40c54163183aeb218716f5dce3f87913e292c61
Luis Arce <luilliarcec@gmail.com>
2020-11-22 16:05:18 -0500

    Method that generates a custom id or returns the past as a parameter

--- src/View/Components/Field.php
+++ src/View/Components/Field.php
@@ -83,4 +83,16 @@ abstract class Field extends Component
     {
         return str_replace('[]', '', $name);
     }
+
+    /**
+     * Gets the id for the input, which can be passed as an
+     * attribute or obtained from the input name
+     *
+     * @param string|null $id
+     * @return string
+     */
+    protected function id(?string $id)
+    {
+        return $id ? $id : sprintf('field-%s', $this->cleanName);
+    }
 }
