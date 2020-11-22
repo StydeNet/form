b0bae79dcf8c30d3f962fccd986ed27c5582e777
Luis Arce <luilliarcec@gmail.com>
2020-11-22 16:36:24 -0500

    Registering RadioComponent in the ServiceProvider

--- src/FormServiceProvider.php
+++ src/FormServiceProvider.php
@@ -54,6 +54,7 @@ class FormServiceProvider extends ServiceProvider
             \Styde\Form\View\Components\Fields\Input::class => 'input',
             \Styde\Form\View\Components\Fields\Textarea::class => 'textarea',
             \Styde\Form\View\Components\Fields\Select::class => 'select',
+            \Styde\Form\View\Components\Fields\Radio::class => 'radio',
             \Styde\Form\Field::class => 'field',
             \Styde\Form\PasswordField::class => 'field-password',
             \Styde\Form\NumberField::class => 'field-number',
