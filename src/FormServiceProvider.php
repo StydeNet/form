317ba9ed2b4fcb9f3fdabb60f24198f9c2c7bc59
Luis Arce <luilliarcec@gmail.com>
2020-11-22 15:13:26 -0500

    Organizing ServiceProvider and moving components within it

--- src/FormServiceProvider.php
+++ src/FormServiceProvider.php
@@ -2,21 +2,65 @@
 
 namespace Styde\Form;
 
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class FormServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/form.php', 'form');
-        $this->mergeConfigFrom(__DIR__.'/../config/components.php', 'components');
+        /*
+         * Optional methods to load your package assets
+         */
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'styde-form');
+        $this->loadJsonTranslationsFrom(__DIR__ . '/../resources/lang');
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views/', 'styde-form');
+        /*
+         * Optional methods to load your package assets
+         */
+        if ($this->app->runningInConsole()) {
+            // Publishing config.
+            $this->publishes([
+                __DIR__ . '/../config/form.php' => config_path('form.php'),
+            ], 'config');
 
-//        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'form');
-        $this->loadJsonTranslationsFrom(__DIR__.'/../resources/lang');
+            // Publishing the views.
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/styde-form'),
+            ], 'views');
+        }
 
-        $this->app->make('blade.compiler')->components(config('components'));
+        $this->app['blade.compiler']->components($this->components());
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/form.php', 'form');
+    }
+
+    /**
+     * Blade components
+     *
+     * @return string[]
+     */
+    private function components()
+    {
+        return [
+            \Styde\Form\Field::class => 'field',
+            \Styde\Form\PasswordField::class => 'field-password',
+            \Styde\Form\NumberField::class => 'field-number',
+            \Styde\Form\SelectField::class => 'field-select',
+            \Styde\Form\CheckboxField::class => 'field-checkbox',
+            \Styde\Form\RadioField::class => 'field-radio',
+            \Styde\Form\TextAreaField::class => 'field-textarea',
+            \Styde\Form\FileField::class => 'field-file',
+            \Styde\Form\SubmitButton::class => 'submit',
+            \Styde\Form\RangeField::class => 'field-range',
+            \Styde\Form\SwitchField::class => 'field-switch',
+            \Styde\Form\FileBrowserField::class => 'field-file-browser',
+        ];
     }
 }
