ddf4ebac33966d9c3ca40ae154164bff4f96d7a1
Luis Arce <luilliarcec@gmail.com>
2020-11-23 09:36:19 -0500

    Adding model User for test with FormInputs

---
+++ tests/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    protected $fillable = ['name', 'email', 'password'];
+
+    protected $hidden = ['password', 'remember_token',];
+}
