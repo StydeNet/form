ff0d1dd9b18e19203f2ee0f39e942cfecd055e26
Luis Arce <luilliarcec@gmail.com>
2020-11-25 18:16:01 -0500

    Adding feedback invalid in Help view component when has errors

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -1,1 +1,5 @@
-<small {{ $attributes->merge(['class' => 'form-text text-muted']) }}>{{ $slot }}</small>
+@if($hasErrors)
+    <div {{ $attributes->merge(['class' => 'invalid-feedback']) }}>{{ $slot }}</div>
+@else
+    <small {{ $attributes->merge(['class' => 'form-text text-muted']) }}>{{ $slot }}</small>
+@endif
