b1036ea807c9d369f9364ccd2bd58a9adf2e1fc6
Luis Arce <luilliarcec@gmail.com>
2020-11-25 17:45:17 -0500

    Adding Tests for highlight

--- tests/Fields/Inputs/TextTest.php
+++ tests/Fields/Inputs/TextTest.php
@@ -20,7 +20,37 @@ class TextTest extends TestCase
         $this->template('<x-input-text name="name"/>')
             ->assertRender('
                 <div id="field-group-name" class="form-group">
+                    <label for="field-name"> Name <span class="badge badge-danger">Optional</span> </label>
+                    <input type="text" id="field-name" name="name" value="" class="form-control">
+                </div>
+            ');
+    }
+
+    /** @test */
+    function render_field_without_highlight()
+    {
+        $this->app['config']->set(['form.highlights_requirement' => null]);
+
+        $this->template('<x-input-text name="name"/>')
+            ->assertRender('
+                <div id="field-group-name" class="form-group">
                     <label for="field-name"> Name </label>
+                    <input type="text" id="field-name" name="name" value="" class="form-control">
+                </div>
+            ');
+    }
+
+    /** @test */
+    function render_field_with_required_highlight()
+    {
+        $this->markTestIncomplete();
+
+        $this->app['config']->set(['form.highlights_requirement' => 'required']);
+
+        $this->template('<x-input-text name="name" required/>')
+            ->assertRender('
+                <div id="field-group-name" class="form-group">
+                    <label for="field-name"> Name <span class="badge badge-danger">Required</span> </label>
                     <input type="text" id="field-name" name="name" value="" class="form-control">
                 </div>
             ');
