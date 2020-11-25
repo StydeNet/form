8f49b3cab0abfc527cc93b93625927320bb5f40e
Luis Arce <luilliarcec@gmail.com>
2020-11-25 17:36:34 -0500

    Using the highlight in input text

--- resources/views/fields/inputs/text.blade.php
+++ resources/views/fields/inputs/text.blade.php
@@ -1,5 +1,5 @@
 <x-form-group :name="$cleanName">
-    <x-label :for="$id">{{ $label }}</x-label>
+    <x-label :for="$id" :highlight="$highlight">{{ $label }}</x-label>
 
     <input type="text" id="{{ $id }}" name="{{ $name }}" value="{{ $value }}"
         {{ $attributes->merge(['class' => $styles($errors->has($cleanName))]) }}>
