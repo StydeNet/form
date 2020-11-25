55ac74e9b1737574a2b574f5f1e9edbbb42894b9
Luis Arce <luilliarcec@gmail.com>
2020-11-25 18:03:32 -0500

    Rename test method

--- tests/HelpTest.php
+++ tests/HelpTest.php
@@ -5,7 +5,7 @@ namespace Tests;
 class HelpTest extends TestCase
 {
     /** @test */
-    function renders_a_form_with_get_method()
+    function renders_a_help_component()
     {
         $this->template('<x-help>This is a help text.</x-help>')
             ->assertRender('<small class="form-text text-muted">This is a help text.</small>');
