5f98665c5adf4e062795f64563bb529639c47a10
Luis Arce <luilliarcec@gmail.com>
2020-11-25 15:24:17 -0500

    Check that a label is rendered

--- tests/LabelTest.php
+++ tests/LabelTest.php
@@ -4,5 +4,9 @@ namespace Tests;
 
 class LabelTest extends TestCase
 {
-
+    function render_a_simple_label()
+    {
+        $this->template('<x-label>Name</x-label>')
+            ->assertRender('<label>Name</label>');
+    }
 }
