b7d753f766ab47bc5e36a8b4a2f8fb14928be712
Luis Arce <luilliarcec@gmail.com>
2020-11-25 18:17:04 -0500

    Test that the help component rendered as feedback error

--- tests/HelpTest.php
+++ tests/HelpTest.php
@@ -10,4 +10,11 @@ class HelpTest extends TestCase
         $this->template('<x-help>This is a help text.</x-help>')
             ->assertRender('<small class="form-text text-muted">This is a help text.</small>');
     }
+
+    /** @test */
+    function renders_a_help_error_component()
+    {
+        $this->template('<x-help has-errors="true">This is a error text.</x-help>')
+            ->assertRender('<div class="invalid-feedback">This is a error text.</div>');
+    }
 }
