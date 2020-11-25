1bf4a05cc1e26628830ba5fcb8f51aca2616b554
Luis Arce <luilliarcec@gmail.com>
2020-11-25 15:36:45 -0500

    Checking that the id is rendered

--- tests/LabelTest.php
+++ tests/LabelTest.php
@@ -8,6 +8,14 @@ class LabelTest extends TestCase
     function render_a_simple_label()
     {
         $this->template('<x-label>Name</x-label>')
-            ->assertRender('<label>Name</label>');
+            ->assertRender('<label >Name</label>');
+    }
+
+    /** @test */
+    function render_a_label_with_id_attribute()
+    {
+        $this->template('<x-label :id="$id">Name</x-label>')
+            ->withData('id', 'field-name')
+            ->assertRender('<label id="field-name">Name</label>');
     }
 }
