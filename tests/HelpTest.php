4f37ca30e78feed8c64383cee21b10dc9fd805af
Luis Arce <luilliarcec@gmail.com>
2020-11-25 18:01:32 -0500

    Refactor helptest component

--- tests/HelpTest.php
+++ tests/HelpTest.php
@@ -7,7 +7,7 @@ class HelpTest extends TestCase
     /** @test */
     function renders_a_form_with_get_method()
     {
-        $this->template('<x-help name="name">This is a help text.</x-help>')
-            ->assertRender('<small id="name-help" class="form-text text-muted">This is a help text.</small>');
+        $this->template('<x-help>This is a help text.</x-help>')
+            ->assertRender('<small class="form-text text-muted">This is a help text.</small>');
     }
 }
