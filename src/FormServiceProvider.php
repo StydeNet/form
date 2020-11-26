738c5675b80cf9271842f9a631f31e1c21a15d36
Luis Arce <luilliarcec@gmail.com>
2020-11-25 20:58:18 -0500

    Creating component copy of input-text as input-search

--- src/FormServiceProvider.php
+++ src/FormServiceProvider.php
@@ -66,6 +66,7 @@ class FormServiceProvider extends ServiceProvider
             \Styde\Form\View\Components\Fields\Inputs\Password::class => 'input-password',
             \Styde\Form\View\Components\Fields\Inputs\Email::class => 'input-email',
             \Styde\Form\View\Components\Fields\Inputs\Url::class => 'input-url',
+            \Styde\Form\View\Components\Fields\Inputs\Search::class => 'input-search',
             \Styde\Form\View\Components\Fields\Input::class => 'input',
             \Styde\Form\View\Components\Fields\Textarea::class => 'textarea',
             \Styde\Form\View\Components\Fields\Select::class => 'select',
