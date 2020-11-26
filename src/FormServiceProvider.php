7f7d875bca81a56c38be2b09daf33f872e6ace95
Luis Arce <luilliarcec@gmail.com>
2020-11-25 22:21:27 -0500

    Register textarea component

--- src/FormServiceProvider.php
+++ src/FormServiceProvider.php
@@ -68,7 +68,7 @@ class FormServiceProvider extends ServiceProvider
             \Styde\Form\View\Components\Fields\Inputs\Url::class => 'input-url',
             \Styde\Form\View\Components\Fields\Inputs\Search::class => 'input-search',
             \Styde\Form\View\Components\Fields\Inputs\File::class => 'input-file',
-            \Styde\Form\View\Components\Fields\Textarea::class => 'textarea',
+            \Styde\Form\View\Components\Fields\Inputs\Textarea::class => 'textarea',
             \Styde\Form\View\Components\Fields\Select::class => 'select',
             \Styde\Form\View\Components\Fields\Radio::class => 'radio',
             \Styde\Form\View\Components\Fields\Checkbox::class => 'checkbox',
