46e81ff8d963165a43293b2a6fdef4dce1b4cf17
Luis Arce <luilliarcec@gmail.com>
2020-11-27 10:21:44 -0500

    Remove old configuration

--- config/form.php
+++ config/form.php
@@ -1,19 +1,5 @@
 <?php
 
 return [
-    'highlights' => [
-        'required' => [
-            'activated' => true,
-            'color-badge' => 'danger',
-            'text' => 'styde-form::field.required'
-        ],
-
-        'optional' => [
-            'activated' => false,
-            'color-badge' => 'info',
-            'text' => 'styde-form::field.optional'
-        ]
-    ],
-
     'highlights_requirement' => 'optional',
 ];
