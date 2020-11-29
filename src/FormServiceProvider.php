cd57290c9d22fbe413ff4592ddba62701280ce17
Luis Arce <luilliarcec@gmail.com>
2020-11-29 14:28:34 -0500

    Registering the Current Model as a singleton in the Services Provider

--- src/FormServiceProvider.php
+++ src/FormServiceProvider.php
@@ -2,21 +2,32 @@
 
 namespace Styde\Form;
 
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Styde\Form\Support\CurrentModel;
 
 class FormServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/form.php', 'form');
-        $this->mergeConfigFrom(__DIR__.'/../config/components.php', 'components');
+        $this->mergeConfigFrom(__DIR__ . '/../config/form.php', 'form');
+        $this->mergeConfigFrom(__DIR__ . '/../config/components.php', 'components');
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views/', 'styde-form');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'styde-form');
 
 //        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'form');
-        $this->loadJsonTranslationsFrom(__DIR__.'/../resources/lang');
+        $this->loadJsonTranslationsFrom(__DIR__ . '/../resources/lang');
 
         $this->app->make('blade.compiler')->components(config('components'));
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Registering Current Model Instance for Forms
+        $this->app->singleton(CurrentModel::class, function ($app) {
+            return new CurrentModel;
+        });
     }
 }
