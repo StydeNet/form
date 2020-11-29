0d0e02edc24a93d5c08e4a0ff38f29d9ca939a38
Luis Arce <luilliarcec@gmail.com>
2020-11-29 10:54:23 -0500

    Adding form-group micro-component

--- config/components.php
+++ config/components.php
@@ -3,6 +3,7 @@
 return [
 
     \Styde\Form\Form::class => 'form',
+    \Styde\Form\View\Components\FormGroup::class => 'form-group',
     \Styde\Form\Field::class => 'field',
     \Styde\Form\PasswordField::class => 'field-password',
     \Styde\Form\NumberField::class => 'field-number',
