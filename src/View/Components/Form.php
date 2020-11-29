1e82fc957656ad2804ef8a17c9a4caec6d69d68a
Luis Arce <luilliarcec@gmail.com>
2020-11-29 14:25:59 -0500

    Organizing and adding phpdoc in Form Class

--- src/View/Components/Form.php
+++ src/View/Components/Form.php
@@ -6,16 +6,34 @@ use Illuminate\View\Component;
 
 class Form extends Component
 {
-    /**
-     * @var string
-     */
+    /** @var string Form method */
     public $method;
 
+    /**
+     * Form constructor.
+     *
+     * @param string $method
+     */
     public function __construct(string $method = 'get')
     {
         $this->method = $method;
     }
 
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return view('styde-form::form');
+    }
+
+    /**
+     * Returns the http method that will go in the form html
+     *
+     * @return string
+     */
     public function httpMethod()
     {
         if ($this->spoofedMethod()) {
@@ -25,13 +43,13 @@ class Form extends Component
         return $this->method;
     }
 
+    /**
+     * Check if the method is put patch or delete
+     *
+     * @return bool
+     */
     public function spoofedMethod()
     {
         return in_array($this->method, ['put', 'patch', 'delete']);
-    }
-
-    public function render()
-    {
-        return view('styde-form::form');
     }
 }
